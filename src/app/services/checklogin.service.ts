00bb8ab0f22bf9891e31ffd1e6adf83275497668
AlexisBouligand <alexisbouligand@gmail.com>
2022-05-29 18:06:14 +0200

    Moved checkLogin and created ressourceService Added some new file on backend

---
+++ src/app/services/checklogin.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckloginService {
+
+  constructor(private http: HttpClient) { }
+
+  private async loginReq(url:any, creds:any){
+    return this.http.post(url + "/login", creds,{
+      withCredentials : true,
+      responseType : "text"
+    }).toPromise();
+  }
+
+  private async verifLoginReq(url:any){
+    return this.http.get(url + "/login",{
+      withCredentials : true,
+      responseType : "text"
+    }).toPromise();
+  }
+
+  async checkAlive(){
+
+    const data = await this.http.get(
+      'http://localhost:4200/api/',
+       {observe: 'response'}
+    ).toPromise().catch(err =>{
+     return err.status;
+    })
+
+    if(data == 504){
+      return false;
+    }else{
+      return true;
+    }
+
+   
+  }
+
+  async checkLogin(): Promise<boolean> {
+    const data = await this.verifLoginReq("http://localhost:4200/api");
+    console.log(data);
+    if(data == "Ok"){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+  async checkLoginCreds(mail:string, pwd:string):Promise<boolean> {
+
+    const url = "http://localhost:4200/api"
+
+    const creds = {
+      "username" : mail,
+      "password" : pwd
+    }
+
+    const data = await this.loginReq(url, creds);
+    console.log(data);
+
+    if(data=="Login succesfull"){
+      return true;
+    }else{
+      return false;
+    }
+
+  
+    
+
+
+    
+
+  }
+}
---
+++ src/app/services/ressource.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Ressource } from 'src/objectClass/Ressource';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RessourceService {
+
+  private apiUrl = 'http://localhost:4200/api';
+ 
+
+  constructor( private http:HttpClient) { }
+
+  
+
+  getRessources(): Observable<Ressource[]>{
+    return this.http.get<Ressource[]>(this.apiUrl);
+  }
+
+  deleteRessource(ressource:Ressource):Observable<Ressource>{
+    const url = `${this.apiUrl}/${ressource.id}`;
+    return this.http.delete<Ressource>(url);
+  }
+/*
+  addRessource(ressource:Ressource):Observable<Ressource>{
+    return this.http.post<Ressource>(this.apiUrl, ressource, httpOptions);
+  }*/
+
+}
